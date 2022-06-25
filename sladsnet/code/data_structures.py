52fe31d17d648de4f204d5e9075b775ef7c014a7
saugat <saugat.kandel@u.northwestern.edu>
2022-06-24 19:19:18 -0500

    Code for experiment.

--- sladsnet/code/data_structures.py
+++ sladsnet/code/data_structures.py
@@ -6,10 +6,10 @@ import numpy as np
 
 @dt.dataclass
 class NeighborsInfo:
-    indices: list
-    distances: list
-    weights: list
-    values: list
+    indices: list = dt.field(default_factory=lambda: [])
+    distances: list = dt.field(default_factory=lambda: [])
+    weights: list = dt.field(default_factory=lambda: [])
+    values: list = dt.field(default_factory=lambda: [])
 
 
 @dt.dataclass
@@ -17,4 +17,5 @@ class MeasurementInfo:
     measured_idxs: np.ndarray = dt.field(default_factory=lambda: np.empty((0, 2), dtype='int'))
     unmeasured_idxs: np.ndarray = dt.field(default_factory=lambda: np.empty((0, 2), dtype='int'))
     measured_values: np.ndarray = dt.field(default_factory=lambda: np.empty(0, dtype='float32'))
+    unnormalized_values: np.ndarray = dt.field(default_factory=lambda: np.empty(0, dtype='float32'))
     new_idxs: list = dt.field(default_factory=lambda: [])
